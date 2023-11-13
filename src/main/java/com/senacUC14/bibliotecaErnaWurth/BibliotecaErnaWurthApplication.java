72aa76d62a8a133adefa06ce5a273612b003c187
JulianoSantosSouza <juliano.souza@canoasedu.rs.gov.br>
2023-11-12 22:18:09 -0300

    primeira versão

---
+++ src/main/java/com/senacUC14/bibliotecaErnaWurth/BibliotecaErnaWurthApplication.java
@@ -0,0 +1,13 @@
+package com.senacUC14.bibliotecaErnaWurth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaErnaWurthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaErnaWurthApplication.class, args);
+	}
+
+}
